eae52fc072a466018eb1d0b9c8c04eb434c9bc11
gmirg <gofthet@gmail.com>
2023-11-28 17:43:33 +0100

    added users profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@
 
 # Controller for users
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
